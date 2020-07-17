666c1f6f054549c38ae7a5ed6aaaa0345341d02b
Andrea Vitali <avitali@infolandsys.com>
2020-07-17 16:52:02 +0200

    Gencounter start

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule ElixirPlayground.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      elixirc_paths: ["lib/gencounter"]
     ]
   end
 
