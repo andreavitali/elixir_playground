53c5a3e3a6b81c0dab1b94b51534ec3c8997fb96
Andrea Vitali <andrea.vitali@anticpay.it>
2020-08-28 17:28:02 +0200

    Fix elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ElixirPlayground.MixProject do
     [
       app: :elixir_playground,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: ["lib/gencounter"]
